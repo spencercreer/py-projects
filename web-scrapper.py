616d4baa37549d0e5783008a5f262dc960bb5376
spencercreer <spencercreer@gmail.com>
2021-07-12 22:26:12 -0600

    scraped repos and write to csv file

--- web-scrapper.py
+++ web-scrapper.py
@@ -1,10 +1,26 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import csv
 
+# prompt user for github username, determine url, and pass to BeautifulSoup
 github_user = input('Input Github Username: ')
 url = 'https://github.com/' + github_user
-
 r = requests.get(url)
 soup = bs(r.content, 'html.parser')
+
+# scrap profile image and pinned repositories
 profile_image = soup.find('img', {'alt': 'Avatar'})['src']
+repositories = soup.find_all('span', {'class': 'repo'})
+
+# open csv file and write header
+file = open('github_users.csv', 'w', newline='', encoding='utf-8')
+writer = csv.writer(file)
+writer.writerow(['profile_image','repositories'])
+
+# print scraped image url and pinned repositories
 print(profile_image)
+for repo in repositories:
+    print(repo.text)
+
+# close csv file
+file.close()
