c8a23d3ebc04091b2010ca145fb9b16f3cb792e2
spencercreer <spencercreer@gmail.com>
2021-09-13 21:19:56 -0700

    updated function to binary search O(logn)

--- binary-search.py
+++ binary-search.py
@@ -2,13 +2,28 @@ import math
 from jovian.pythondsa import evaluate_test_cases
 
 #PROBLEM: Alice has some cards with numbers written on them. She arranges the cards in decreasing order, and lays them out face down in a sequence on a table. She challenges Bob to pick out the card containing a given number by turning over as few cards as possible. Write a function to help Bob locate the card.
+def test_location(cards, query, mid):
+    if cards[mid] == query:
+        if mid-1 >= 0 and cards[mid-1] == query:
+            return 'left'
+        else:
+            return 'found'
+    elif cards[mid] < query:
+        return 'left'
+    else:
+        return 'right'
 
 def locate_card(cards, query):
-    position = 0
-    while position < len(cards):
-        if cards[position] == query:
-            return position
-        position += 1
+    lo, hi = 0, len(cards) - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        result = test_location(cards, query, mid)
+        if result == 'found':
+            return mid
+        elif result == 'left':
+            hi = mid - 1
+        elif result == 'right':
+            lo = mid + 1
     return -1
 
 tests = []
@@ -60,7 +75,7 @@ tests.append({
     },
     'output': -1
 })
-# numbers can repeat in cards
+#6 numbers can repeat in cards
 tests.append({
     'input': {
         'cards': [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
@@ -68,13 +83,21 @@ tests.append({
     },
     'output': 7
 })
-# query occurs multiple times
+#7 query occurs multiple times
 tests.append({
     'input': {
         'cards': [8, 8, 6, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
         'query': 6
     },
     'output': 2
+})
+#large test
+tests.append({
+    'input': {
+        'cards': list(range(10000000, 0, -1)),
+        'query': 2
+    },
+    'output': 9999998
 })
 
 evaluate_test_cases(locate_card, tests)
