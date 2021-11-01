23d8c097a17535e1ad5a40c9287d535bc7e88fe7
spencercreer <spencercreer@gmail.com>
2021-11-01 08:23:57 -0600

    future value graphics window

---
+++ README.md
@@ -0,0 +1,25 @@
+# Python Projects
+
+## Binary Search
+Test cases for a binary search problem
+
+## Future Value
+An investment future value calculator with graphic window
+
+## News Email
+An automatic email generator that sends the Hacker News headlines
+
+## NumPy
+Common NumPy methods
+
+## OOP
+Principals of object oriented programming
+
+## QR Code
+Generate QR code .png files
+
+## Weather
+Get the weather for an entered city
+
+## Web Scrapper
+A beautiful soup web scrapper that gathers information on an entered GitHub repository
--- future-value/future-value.py
+++ future-value/future-value.py
@@ -1,13 +1,35 @@
+from graphics import *
+
+# Introduction
 print("This program calculates the future value of an investment")
 
-pv = eval(input("Enter the principal: "))
-interest = eval(input("Enter the annual intrest earned as a decimal: "))
-yr = eval(input("Enter the number of years: "))
+# Get principal and interest rate
+pv = float(input("Enter the principal: "))
+interest = float(input("Enter the annual intrest earned as a decimal: "))
+yr = 10
 
+# Create graphics window with labels
+win = GraphWin('Investment Growth Chart', 320, 240)
+win.setBackground('white')
+win.setCoords(-1.75, -200, 11.5, 10400)
+Text(Point(-1, 0), '0.0K').draw(win)
+Text(Point(-1, 2500), '2.5K').draw(win)
+Text(Point(-1, 5000), '5.0K').draw(win)
+Text(Point(-1, 7500), '7.5K').draw(win)
+Text(Point(-1, 10000), '10.0K').draw(win)
 
-for i in range(yr):
+# Draw bar for initial principal
+bar = Rectangle(Point(0, 0), Point(1, pv))
+bar.setFill('green')
+bar.setWidth(2)
+bar.draw(win)
+
+for year in range(1, 11):
     pv = pv * (1 + interest)
+    bar = Rectangle(Point(year, 0), Point(year+1, pv))
+    bar.setFill('green')
+    bar.setWidth(2)
+    bar.draw(win)
 
-print("Future Value" + pv)
-print("Annual Interest Rate" + interest)
-print("Number of Years" + yr)
+input("Press <Enter> to quit")
+win.close()
